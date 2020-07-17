4f4e3a1c3340f124ccbfe35e49c853c5ec0b9365
JW9506 <cf.dota@gmail.com>
2020-07-16 23:20:35 -0400

    init

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require("gulp")
+const browserSync = require("browser-sync").create()
+const postcss = require("gulp-postcss")
+
+function copy() {
+  return gulp.src("src/favicon.ico").pipe(gulp.dest("./build"))
+}
+
+function css() {
+  return gulp
+    .src("src/styles.css")
+    .pipe(postcss([require("tailwindcss"), require("autoprefixer")]))
+    .pipe(gulp.dest("./build"))
+}
+
+function html() {
+  browserSync.reload()
+  return gulp.src("src/index.html").pipe(gulp.dest("./build"))
+}
+
+function sync() {
+  browserSync.init({
+    server: {
+      baseDir: "./build",
+    },
+  })
+  gulp.watch("src/index.html", html)
+}
+
+const build = gulp.series(copy, css, html, sync)
+
+exports.default = build
