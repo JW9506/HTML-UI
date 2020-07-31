c3892b3bbf6aaf74d5af15264c91639ffea8605b
JW9506 <cf.dota@gmail.com>
2020-07-31 19:12:06 -0400

    chore: watch styles.css, enable tailwind

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,47 @@
-const gulp = require("gulp")
-const browserSync = require("browser-sync").create()
-const postcss = require("gulp-postcss")
-const purgecss = require("gulp-purgecss")
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const postcss = require('gulp-postcss')
+const purgecss = require('gulp-purgecss')
 
 function copy() {
-  return gulp.src("src/favicon.ico").pipe(gulp.dest("./build"))
+  return gulp.src('src/favicon.ico').pipe(gulp.dest('./build'))
 }
 
 function css() {
   let src = gulp
-    .src("src/styles.css")
-    .pipe(postcss([require("tailwindcss"), require("autoprefixer")]))
-  if (process.env === "production") {
+    .src('src/styles.css')
+    .pipe(postcss([require('tailwindcss'), require('autoprefixer')]))
+  if (process.env === 'production') {
     src = src.pipe(
       purgecss({
-        content: ["src/**/*.html"],
+        content: ['src/**/*.html'],
       })
     )
   }
-  return src.pipe(gulp.dest("./build"))
+  return src.pipe(gulp.dest('./build'))
 }
 
 function html() {
   browserSync.reload()
-  return gulp.src("src/index.html").pipe(gulp.dest("./build"))
+  return gulp.src('src/index.html').pipe(gulp.dest('./build'))
 }
 
 function sync() {
   browserSync.init({
     server: {
-      baseDir: "./build",
+      baseDir: './build',
     },
     notify: false,
   })
-  gulp.watch("src/index.html", html)
-  gulp.watch("./tailwind.config.js", gulp.series(css, html))
+  gulp.watch('src/index.html', html)
+  gulp.watch(
+    ['./tailwind.config.js', './src/styles.css'],
+    gulp.series(css, html)
+  )
 }
 
 const build = gulp.series(
-  ...[copy, css, html, process.env !== "production" && sync].filter(Boolean)
+  ...[copy, css, html, process.env !== 'production' && sync].filter(Boolean)
 )
 
 exports.default = build
