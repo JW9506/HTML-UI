b09ccf038077e12034416b020181ce8baa0709d5
JW9506 <cf.dota@gmail.com>
2020-08-03 23:54:47 -0400

    chore: lint and add tailwind intellisense

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,31 @@
-const gulp = require("gulp")
-const browserSync = require("browser-sync").create()
-const postcss = require("gulp-postcss")
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const postcss = require('gulp-postcss')
 
 function copy() {
-  return gulp.src("src/favicon.ico").pipe(gulp.dest("./build"))
+  return gulp.src('src/favicon.ico').pipe(gulp.dest('./build'))
 }
 
 function css() {
   return gulp
-    .src("src/styles.css")
-    .pipe(postcss([require("tailwindcss"), require("autoprefixer")]))
-    .pipe(gulp.dest("./build"))
+    .src('src/styles.css')
+    .pipe(postcss([require('tailwindcss'), require('autoprefixer')]))
+    .pipe(gulp.dest('./build'))
 }
 
 function html() {
   browserSync.reload()
-  return gulp.src("src/index.html").pipe(gulp.dest("./build"))
+  return gulp.src('src/index.html').pipe(gulp.dest('./build'))
 }
 
 function sync() {
   browserSync.init({
     server: {
-      baseDir: "./build",
+      baseDir: './build',
     },
     notify: false,
   })
-  gulp.watch("src/index.html", html)
+  gulp.watch('src/index.html', html)
 }
 
 const build = gulp.series(copy, css, html, sync)
